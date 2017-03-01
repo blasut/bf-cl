5bfb075872a66adfcbccda5ba40f0eacfce696ac
blasut <blasut937@gmail.com>
2017-03-01 17:02:32 +0100

    add sumper simple model and run first program

--- bf.lisp
+++ bf.lisp
@@ -29,20 +29,43 @@
 (defun make-cells ()
   (make-array 8 :initial-element 0))  
 
+(defclass model ()
+  ((cells
+    :initarg :cells
+    :initform (make-cells)
+    :accessor cells)
+   (pointer
+    :initform 0
+    :accessor pointer)))
+
 (defun parse-commands (string)
   (loop :for char :across string
      collect (parse-command char)))
 
 (defun parse-command (char)
-  (cond ((char= char #\+) #'1+)
-        (t (error "Unknown brainfuck command"))))
+  (cond ((char= char #\+) #'command-increase)
+        ((char= char #\-) #'command-decrease)
+        (t (error "Unknown brainfuck command: ~@c" char))))
+
+(defun command-increase (model)
+  (setf (aref (cells model) (pointer model)) ;; position
+        (1+ (aref (cells model) (pointer model))))) ;; new value
+
+(defun command-decrease (model)
+  (setf (aref (cells model) (pointer model)) ;; position
+        (1- (aref (cells model) (pointer model))))) ;; new value
 
 (defun bf (bf-string)
-  (let ((cells (make-cells))
+  (let ((model (make-instance 'model))
         (commands (parse-commands bf-string)))
-    (list commands cells)))
+    (pprint (list commands (cells model)))
+    (progn
+      (loop :for command :in commands
+         :do (funcall command model))
+      model)))
 
-(pprint (bf "++"))
+(pprint (parse-commands "++-"))
+(pprint (bf "++-"))
 
-(assert (bf "++") (make-array 8 :initial-contents '(2 0 0 0 0 0 0 0)))
+(assert (equalp (cells (bf "++")) (cells (make-instance 'model :cells (make-array 8 :initial-contents '(2 0 0 0 0 0 0 0))))))
 
